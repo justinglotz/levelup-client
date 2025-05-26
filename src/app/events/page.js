4bf97f015f2aaf90fa3690791cb990d9180ab4f8
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-05-26 10:16:52 -0500

    new event form, new event button

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,11 +1,14 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/navigation';
 import EventCard from '../../components/event/eventCard';
 import { getEvents } from '../../utils/data/eventData';
 
 function Home() {
   const [events, setEvents] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getEvents().then((data) => setEvents(data));
@@ -13,6 +16,13 @@ function Home() {
 
   return (
     <article className="events">
+      <Button
+        onClick={() => {
+          router.push('/events/new');
+        }}
+      >
+        Register New Event
+      </Button>
       <h1>Events</h1>
       {events.map((event) => (
         <section key={`event--${event.id}`} className="event">
