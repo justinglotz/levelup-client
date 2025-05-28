f3f5667ac6cce8d16214ca9b870f6b86510f15c2
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-05-27 19:00:58 -0500

    added update event form, works in backend

--- src/utils/data/eventData.js
+++ src/utils/data/eventData.js
@@ -21,5 +21,31 @@ const createEvent = (event) =>
       .then((data) => resolve(data))
       .catch(reject);
   });
+
+const updateEvent = (event) =>
+  new Promise((resolve, reject) => {
+    fetch(`${clientCredentials.databaseURL}/events/${event.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(event),
+    })
+      .then((response) => resolve(response))
+      .catch(reject);
+  });
+
+const getSingleEvent = (eventId) =>
+  new Promise((resolve, reject) => {
+    fetch(`${clientCredentials.databaseURL}/events/${eventId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch(reject);
+  });
 // eslint-disable-next-line import/prefer-default-export
-export { getEvents, createEvent };
+export { getEvents, createEvent, updateEvent, getSingleEvent };
